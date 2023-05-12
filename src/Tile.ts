b1221861594803042cad801784ebcb740711d1aa
Mark Benson <mark@sub-design.co.uk>
2023-05-12 20:06:39 +0100

    Remove comments

--- src/Tile.ts
+++ src/Tile.ts
@@ -27,7 +27,7 @@ class Tile {
     // calculate that based on the size of the tile and the number of lines we want
     const calcRadii = (size: number, qty: number): number[] => {
       const spacing = size / qty;
-      console.log("CalcRadii, size", size, "quantity", qty, "spacing", spacing);
+      // console.log("CalcRadii, size", size, "quantity", qty, "spacing", spacing);
       let arcRadii = [];
       for (let i = 0; i <= qty; i++) {
         arcRadii.push(0 + i * spacing); // add each position to the array
@@ -35,8 +35,8 @@ class Tile {
       return arcRadii;
     };
 
-    // const direction = Math.random() < 0.5 ? "horizontal" : "vertical";
-    const direction = "horizontal";
+    const direction = Math.random() < 0.5 ? "horizontal" : "vertical";
+    // const direction = "horizontal";
 
     const radii = calcRadii(this.tileSize, this.numberOfLines);
 
@@ -50,6 +50,42 @@ class Tile {
     if (this.showArcs) {
       const arcCenter = { x: 0, y: 0 };
       const largestRadius = radii[radii.length - 1];
+
+      // If we are using 'paths' for straight lines, we need to query the SVG like this:
+      // querySelectorAll('svg path:where([d^="M"]):is([d*=" L"])');
+      Array.from(
+        group.querySelectorAll('path:where([d^="M"]):is([d*=" L"])')
+      ).forEach((line) => {
+        const dAtt = line.getAttribute("d");
+        // console.log("d", dAtt);
+        const dAttParts = dAtt?.split(" ");
+        // console.log("parts", dAttParts);
+        const lineStart = {
+          x: Number(dAttParts?.[1]),
+          y: Number(dAttParts?.[2]),
+        };
+        const lineEnd = {
+          x: Number(dAttParts?.[4]),
+          y: Number(dAttParts?.[5]),
+        };
+        // console.log("start coords", lineStart, "end coords", lineEnd);
+
+        // Determine if there is an intersection for the current line
+        const intersection = this.lineArcIntersection(
+          lineStart,
+          lineEnd,
+          arcCenter,
+          largestRadius
+        );
+
+        // console.log("Intersection", intersection);
+        if (intersection) {
+          line.setAttribute(
+            "d",
+            `M ${intersection.x} ${intersection.y} L ${lineEnd.x} ${lineEnd.y}`
+          );
+        }
+      });
 
       // Convert the linesGroup.lines into an array
       Array.from(group.querySelectorAll("line")).forEach((line) => {
@@ -72,11 +108,11 @@ class Tile {
         );
 
         if (intersection) {
-          console.log("Intersection found", intersection);
+          // console.log("Intersection found", intersection);
           line.setAttribute("x1", String(intersection.x));
           line.setAttribute("y1", String(intersection.y));
         } else {
-          console.log("No intersection found");
+          // console.log("No intersection found");
         }
       });
 
@@ -90,12 +126,30 @@ class Tile {
   }
 
   createLine(radius: number, direction: "horizontal" | "vertical"): SVGElement {
-    console.log(
-      "Creating a single line with direction",
-      direction,
-      "and length",
-      radius
+    // console.log(
+    //   "Creating a single line with direction",
+    //   direction,
+    //   "and length",
+    //   radius
+    // );
+
+    // Try making this a path instead of a line
+    const linePath = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
     );
+    // d="M 10 10 L 150 80 Q 100 20 180 50"
+    let d;
+    if (direction === "horizontal") {
+      d = ["M", "0", String(radius), "L", "100", String(radius)].join(" ");
+    } else {
+      d = ["M", String(radius), "0", "L", String(radius), 100].join(" ");
+    }
+
+    linePath.setAttribute("d", d);
+    linePath.setAttribute("fill", "none");
+    linePath.setAttribute("stroke", "black");
+    linePath.setAttribute("stroke-width", "1");
 
     const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
@@ -120,7 +174,7 @@ class Tile {
     line.setAttribute("stroke", "black");
     line.setAttribute("stroke-width", "1");
 
-    return line;
+    return linePath;
   }
 
   // return an SVG arc for the given start coordinates and radius between the start and end angle
